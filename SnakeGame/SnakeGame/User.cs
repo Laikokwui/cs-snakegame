d0b5b024f0b23b1f64a0a46ae5627d9199cb0fb7
Lai Kok Wui <kokwei325@gmail.com>
2020-11-03 21:25:19 +0800

    Add User Class Object

---
+++ SnakeGame/SnakeGame/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeGame
+{
+    class User
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+        public double Time { get; set; }
+
+        public User(string name)
+        {
+            Name = name;
+            Score = 0;
+        }
+
+        // increase the user score
+        public void AddScore(int score)
+        {
+            Score += score;
+        }
+    }
+}
